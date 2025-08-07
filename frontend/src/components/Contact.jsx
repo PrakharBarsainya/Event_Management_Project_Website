c47a1eafdde88ec7ac3bb966d21c9a1c26fad32a
PRAKHAR BARSAINYA <148670847+PrakharBarsainya@users.noreply.github.com>
2025-08-07 23:18:18 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://event-management-project-website.onrender.com",
         {
           name,
           email,
